891ed6d2f8bd42e7e6b0ae0b48a7c309761ee4d6
Kimberly Wong <kjowong@gmail.com>
2017-02-08 20:02:48 +0000

    Adding solutions to 0, 1, 2, 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+  * main - prints string
+  *
+  * Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
