7845b6a82d859be96936f60b2f0ba78a695bdfdd
Kimberly Wong <kjowong@gmail.com>
2017-02-08 22:57:22 +0000

    Testing solution 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+  * main -
+  *
+  * Return 0 (Success)
+**/
+int main(void)
+{
+	int integerType;
+	float floatType;
+	char charType;
+	long longType;
+	long long longlongType;
+
+	printf("Size of a char: %ld bytes\n",sizeof(charType));
+	printf("Size of an int: %ld bytes\n",sizeof(intType));
+	printf("Size of a long int: %ld bytes\n",sizeof(longType));
+	printf("Size of a long long int: %ld bytes\n ",sizeof(longlongType));
+	printf("Size of a float: %ld bytes\n",sizeof(floatType));
+	return (0);
+}
