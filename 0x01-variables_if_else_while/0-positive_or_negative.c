350c97efb9100e5555afcb8bede52195529926fd
Kimberly Wong <kjowong@gmail.com>
2017-02-09 20:22:24 +0000

    Adding solution to 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/* more headers goes there */
+/**
+ * main - main initial block
+ * Function: prints statement based on if random number is greater, less then
+ * or equal to zero
+ * Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+}
