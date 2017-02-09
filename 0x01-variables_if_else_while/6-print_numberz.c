ab873e79d6c599cbc1fb755094892a7afcc08fc0
Kimberly Wong <kjowong@gmail.com>
2017-02-09 23:58:51 +0000

    Adding solution to 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+/**
+  * main - initial block
+  *
+  * Return: 0
+**/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
