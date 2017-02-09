b60296df430b0433c92da80a8b526e33f4a6e433
Kimberly Wong <kjowong@gmail.com>
2017-02-09 21:13:13 +0000

    Switching statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,13 @@ int main(void)
 
 	ln = n % 10;
 
-	if (ln > 5)
+	if (ln < 6 && ln != 0)
+	{
+		printf("Last digit %d is %d and is less than 6 and not 0\n", n, ln);
+	}
+	else if (ln > 5)
 	{
 		printf("Last digit %d is %d and is greater than 5\n", n, ln);
-	}
-	else if (ln < 6 && ln != 0)
-	{
-		printf("Last digit %d is %d and less than 6 and not 0\n", n, ln);
 	}
 	else
 	{	printf("Last digit %d is %d and is 0\n", n, ln);
