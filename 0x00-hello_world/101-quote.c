9a288896d2a2676c9c7a5c9790f288c7dc87fb12
Kimberly Wong <kjowong@gmail.com>
2017-02-09 05:33:06 +0000

    Changing ode

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,9 @@
 *
 * Return: Always 1 (Success)
 **/
-
 int main(void)
 {
-
-	const char 
-	write(2, "and that piece of art is useful\"" " - Dora Korpar, 2015-10-19\n", 4);
+	int st = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size);
 	return (1);
 }
