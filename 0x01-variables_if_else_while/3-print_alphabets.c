289cf63c5dadc949fa444163eddd75a3cb2c90a7
Kimberly Wong <kjowong@gmail.com>
2017-02-09 22:29:05 +0000

    Adding solution to 4

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+  * main - initial block
+  *
+  * Return: 0
+**/
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		for (d = 'A'; d <= 'Z'; d++)
+	putchar(d);
+	putchar('\n');
+	return (0);
+}
