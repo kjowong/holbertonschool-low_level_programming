766185c9cfc540f46aee5b59af2a7277a6c5d3c1
Kimberly Wong <kjowong@gmail.com>
2017-02-09 22:15:49 +0000

    Adding solution to 3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+/**
+  * main - main initial block
+  * Function
+  * Return: 0 (Success)
+**/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
