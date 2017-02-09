2c77a9234413c5980036086da6a12819febd440f
Kimberly Wong <kjowong@gmail.com>
2017-02-09 05:11:57 +0000

    Partial code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+* main - print string
+*
+* Return: Always 1 (Success)
+**/
+
+int main(void)
+{
+
+	const char 
+	write(2, "and that piece of art is useful\"" " - Dora Korpar, 2015-10-19\n", 4);
+	return (1);
+}
