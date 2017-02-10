1a80be78d1c1ed1eab1d36206e01f6742b6f1444
Kimberly Wong <kjowong@gmail.com>
2017-02-10 01:45:50 +0000

    Adding solution to 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+/**
+  * main - initial block
+  *
+  * Return: 0
+  **/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 100; i++)
+	{
+		putchar(i / 10 % 10 + '0');
+		putchar(i % 10 + '0');
+		if (i < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
