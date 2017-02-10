09795cda22502891a301c9721fa3c2dcbf199866
Kimberly Wong <kjowong@gmail.com>
2017-02-10 04:46:29 +0000

    retesting

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
-/* This program prints characters from 0 to 9 with
-    * the help of putchar function.
-     */
-
 /**
-   * main - Entry point
-    *
-     * Return: Always 0 (Success)
-      */
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+ */
 int main(void)
 {
-		int i;
+	int i;
 
-		for (i = 0; i < 10; i++)
-		{
-			putchar(i % 10 + '0');
-		}
+	for (i = 0; i < 10; i++)
+	{
+	putchar(i + '0');
+	}
 
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }
