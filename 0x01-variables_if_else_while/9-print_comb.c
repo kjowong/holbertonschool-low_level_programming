ca9438801b84203acd2450320c1e0ace71b3823a
Kimberly Wong <kjowong@gmail.com>
2017-02-10 01:12:45 +0000

    Adding solution to 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+/**
+  * main - initial block
+  *
+  * Return: 0
+  **/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
