acad31d2483f1cf2d7e759a8db3d86506b78c031
Kimberly Wong <kjowong@gmail.com>
2017-02-10 06:17:05 +0000

    Adding what I have

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+  * main - initial block
+  *
+  * Return: 0
+  **/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+		if (i < i)
+		{
+		continue;
+		}
+		putchar(i / 10 % 10 + 48);
+		putchar(i % 10 + 48);
+
+		putchar(' ');
+
+
+
+			putchar(j / 10 % 10 + 48);
+			putchar(j % 10 + 48);
+			putchar(',');
+			putchar(' ');
+		}
+		}
+
+
+
+	putchar('\n');
+	return (0);
+}
