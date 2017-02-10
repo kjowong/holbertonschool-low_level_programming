edcb872895385ff44df54d3346b9053f75028b35
Kimberly Wong <kjowong@gmail.com>
2017-02-10 00:32:39 +0000

    Adding solution to 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+/**
+  * main - initial block
+  *
+  * Return: 0
+  **/
+
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+
+	while (i < 10)
+	{
+		putchar(i % 10 + '0');
+		i++;
+	}
+	while (c < 'g')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
