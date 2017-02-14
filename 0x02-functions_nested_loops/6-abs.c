ef66b6da8168e9aec884a6b74e5e1287a66c5a00
Kimberly Wong <kjowong@gmail.com>
2017-02-14 21:17:43 +0000

    Removing return

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -11,14 +11,13 @@ int _abs(int n)
 	{
 		return (1);
 	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
+	else if (n < 0)
 	{
 		return (n * -1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 
 }
