d7de0d6d796dcc0f0ac41d848c0315b8c14e82e0
Kimberly Wong <kjowong@gmail.com>
2017-02-15 00:33:01 +0000

    Adding solution to 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,23 +10,33 @@ void times_table(void)
 	int j;
 	int k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			k = i * j;
-			_putchar(k / 10 + '0');
+			if (j == 0)
+			{
+				_putchar(k + '0');
+
+			}
+			else if (k <= 9)
+			{
 			_putchar(',');
 			_putchar(' ');
-			if (k > 9)
+			_putchar(' ');
+			_putchar(k + '0');
+			}
+			else if (k >= 10)
 			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
 				_putchar(',');
 				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 
 		}
+		_putchar('\n');
 
 	}
 
