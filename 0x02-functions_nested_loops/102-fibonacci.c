e7f522534ac22a08071310eb12b3c2fd785b6d0d
Kimberly Wong <kjowong@gmail.com>
2017-02-15 23:56:57 +0000

    Adding solution to 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+  * main - initial block
+  * @void: no argument
+  * Return: 0
+  */
+int main(void)
+{
+unsigned long int curr, prev, sum, counter;
+
+ prev = 1;
+ curr = 2;
+ sum = 0;
+
+printf("%lu, ", prev);
+printf("%lu, ", curr);
+
+ for (counter = 2; counter < 50; counter++)
+ {
+		 sum = prev + curr;
+		 prev = curr;
+ 		 curr = sum;
+		if (counter != 49)
+		{
+			printf("%lu, ", sum);
+		}
+		else
+		{
+			 printf("%lu\n", sum);
+		}
+ }
+	 return (0);
+
+}
