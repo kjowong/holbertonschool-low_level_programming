cb8804f20eb36349d3a206e27168397e26cbd0ba
Kimberly Wong <kjowong@gmail.com>
2017-02-15 21:53:29 +0000

    Adding solution to 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,18 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * print_times_table - prints the times table
+  * @n: takes in an integer
   *
   */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-	{
-		return;
-	}
-	else
-	{
-
 	int i, j, k;
 
-	for (i = 0; i <=n; i++)
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <=n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			k = i * j;
 			if (j == 0)
@@ -29,18 +24,26 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(':');
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			else if (k >= 10)
+			else if (k >= 10 && k < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(k / 10 % 10 + '0');
 				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
 	}
-	}
-
 }
