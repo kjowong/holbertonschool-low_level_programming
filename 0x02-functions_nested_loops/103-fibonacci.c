ecf703d285334a0c9f4706680c69136f4dcc456d
Kimberly Wong <kjowong@gmail.com>
2017-02-16 00:33:49 +0000

    Adding solution to 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+  * main - initial block
+  * @void: no argument
+  * Return: 0
+  */
+int main(void)
+{
+	unsigned long int first, second, next, even;
+
+	first = 1;
+	second = 2;
+	next = 0;
+	even = 0;
+
+	while (next < 4000000)
+	{
+		next = first + second;
+		first = second;
+		second = next;
+		if (next % 2 == 0)
+		{
+			even += next;
+		}
+	}
+	printf("%lu\n", even);
+	return (0);
+
+}
