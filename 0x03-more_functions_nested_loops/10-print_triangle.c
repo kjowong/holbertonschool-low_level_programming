f59fd660406fe51504b779200474950b1ed09a11
Kimberly Wong <kjowong@gmail.com>
2017-02-16 23:00:21 +0000

    Adding 10

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+/**
+  * print_triangle - prints a triangle
+  * @size: takes in a integer for size of triangle
+  *
+  */
+void print_triangle(int size)
+{
+	int column, row;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (column = 0; column < size; column++)
+	{
+		for(row = 0; row <= column; row++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
