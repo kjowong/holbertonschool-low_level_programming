ba65bdc4938703186ba40a0bb56aad7e0b7c54d2
Kimberly Wong <kjowong@gmail.com>
2017-02-16 22:01:29 +0000

    Adding solution

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,2 +1,32 @@
-#include "holberton.h"
+#include <stdio.h>
+/**
+  * main - initial block
+  * @void: no argument
+  *
+  */
+int main(void)
+{
+	int i;
 
+	for (i = 1; i <= 100; i++)
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	printf("\n");
+	return (0);
+}
