79e6a7f7500093ec1f4c3c1060653c0f330d265e
Kimberly Wong <kjowong@gmail.com>
2017-02-17 01:07:20 +0000

    Adding 100

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - initial block
+  * @void: no argument
+  *
+  */
+int main(void)
+{
+	int i, j;
+
+	for (i = 2; i <= i; i++)
+	{
+		if(n % 1 == 0)
+		{
+			printf("%d", i);
+			n = n / i;
+			--i;
+		}
+	}
+
+
+}
