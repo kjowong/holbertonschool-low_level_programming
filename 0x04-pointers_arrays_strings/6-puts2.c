d321a3ef98b0ddd74f072bfb5d0f028949f85e07
Kimberly Wong <kjowong@gmail.com>
2017-02-21 17:03:02 +0000

    Adding 6/7

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,17 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * void puts2 - prints one character out of two
+  * @str: takes in a string
   *
   */
+void puts2(char *str)
+{
+	int i;
 
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,37 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * puts_half - prints half of a string
+  * @str: takes in a string
   *
   */
+void puts_half(char *str)
+{
+	int i, half;
+	int c = 0;
 
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[c] % 2 == 1)
+		{
+			_putchar(str[i] - 1);
+		}
+		else
+		{
+			half = str[c] / 2;
+			for (j = 0; j < half; j++)
+			{
+				temp = str[c];
+				str[i] = temp;
+			}
+			temp[i] = '\0';
+			_putchar(half);
+		}
+	}
+	_putchar('\n');
+}
