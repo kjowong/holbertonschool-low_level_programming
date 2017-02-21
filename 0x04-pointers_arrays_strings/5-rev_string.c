40e16551ff8742242e69bb5df4969eceb0f37667
Kimberly Wong <kjowong@gmail.com>
2017-02-21 02:30:46 +0000

    Adding solution

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,25 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * rev_string - reverse a string
+  * @s: takes in string
   *
   */
+void rev_string(char *s)
+{
+	int i, temp;
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	c--;
+
+	for (i = 0; c > 0; c--, i++)
+	{
+		temp = s[i];
+		s[i] = s[c];
+		s[c] = temp;
+	}
+}
 
