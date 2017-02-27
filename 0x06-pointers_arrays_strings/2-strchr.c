d135455199f03b2db68217a2d84075d25a6644d4
Kimberly Wong <kjowong@gmail.com>
2017-02-27 20:40:52 +0000

    Adding solutions

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,20 @@
 #include "holberton.h"
 /**
-  *
-  *
-  *
+  * _strchr - locates character in a string
+  * @s: string
+  * @c: character
+  * Return: pointer s value
   */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+	}
+	return (s + i);
+}
