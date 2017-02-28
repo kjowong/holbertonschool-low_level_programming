2dc43382a7fe92b1f67f44e06a3f3ff8dcae1099
Kimberly Wong <kjowong@gmail.com>
2017-02-28 05:24:38 +0000

    Going back

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,6 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	if (needle == NULL || haystack == NULL)
-	{
-		return (NULL);
-	}
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
@@ -27,10 +23,10 @@ char *_strstr(char *haystack, char *needle)
 				break;
 			}
 		}
-	}
 	if (needle[j] == '\0')
 	{
 			return (haystack);
+	}
 	}
 	return (NULL);
 }
