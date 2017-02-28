706a876e01ce6e05bbaea5c1afe13be0f0deb984
Kimberly Wong <kjowong@gmail.com>
2017-02-28 01:14:41 +0000

    Adding

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,32 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-  *
-  *
-  *
+  * _strstr - function that locates a substring
+  * @haystack: string that is being compared
+  * @needle: substring that is being located
+  * Return: null is nothing is found
   */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle == NULL || haystack == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (needle[j] == haystack[i])
+			{
+				return (haystack + i);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return ('\0');
+}
