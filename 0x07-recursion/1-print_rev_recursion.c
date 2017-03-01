57f24ca55238dcac705685899b8423f7206da42e
Kimberly Wong <kjowong@gmail.com>
2017-03-01 20:39:07 +0000

    Adding solutions

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,7 +1,15 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * _print_rev_recursion - prints string reverse recursively
+  * @s: takes in a string
   *
   */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
 
--- 0x07-recursion/3-factorial.c
+++ 0x07-recursion/3-factorial.c
@@ -1,7 +1,19 @@
 #include "holberton.h"
 /**
-  *
-  *
-  *
+  * factorial - returns the factorial of a given number
+  * @n: takes in an integer
+  * Return: n * n - 1 so it * to the number less
   */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
 
