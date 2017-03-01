1bb6dc99c87b5d41bff47cf60647fe864cb039c2
Kimberly Wong <kjowong@gmail.com>
2017-03-01 20:33:52 +0000

    Adding 0

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,7 +1,19 @@
 #include "holberton.h"
 /**
-  *
-  *
+  * _puts_recursion - prints string recursively
+  * @s: takes in a string
   *
   */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
 
