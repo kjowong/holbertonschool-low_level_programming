e47785d773c2a2318f2cec028b5ba6c377786c7f
Kimberly Wong <kjowong@gmail.com>
2017-03-01 20:43:32 +0000

    Adding descrip

--- 0x07-recursion/2-strlen_recursion.c
+++ 0x07-recursion/2-strlen_recursion.c
@@ -2,13 +2,13 @@
 /**
   * _strlen_recursion - returns the length of a string
   * @s: takes in a string
-  *
+  * Return: adding 1 everytime
   */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	return (1 + _strlen_recursion(s + 1));
 }
