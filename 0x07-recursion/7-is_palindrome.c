2d2d8a50f394ecd28b6c7b7b0a3fe73665df6ab0
Kimberly Wong <kjowong@gmail.com>
2017-03-02 01:42:33 +0000

    Adding 7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -1,7 +1,54 @@
 #include "holberton.h"
 /**
-  *
-  *
-  *
+  * is_palindrome - returns 1 if string is palindrome & 0 if not
+  * @s: takes in a string
+  * Return: return check if palindrome function
   */
+int is_palindrome(char *s)
+{
+	int length;
+
+	length = _strlen_recursion(s);
+	return (_check(s, length - 1));
+}
+
+/**
+  * _strlen_recursion - string length
+  * @s: takes in string
+  * Return: return string length func
+  */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+  * _check - checks if palindrome
+  * @s: takes in string
+  * @l: length
+  * Return: check if palindrome
+  */
+
+int _check(char *s, int l)
+{
+	if (l == 1 || l == 0)
+	{
+		return (1);
+	}
+	if (*s != s[l])
+	{
+		return (0);
+	}
+	else
+	{
+		return (_check(s + 1, l - 2));
+	}
+
+}
 
