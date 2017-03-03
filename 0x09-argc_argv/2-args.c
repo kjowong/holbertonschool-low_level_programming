499d7d7e899c4b7ca7fd328faff140fa0074075c
Kimberly Wong <kjowong@gmail.com>
2017-03-03 23:35:51 +0000

    Adding 2

--- 0x09-argc_argv/2-args.c
+++ 0x09-argc_argv/2-args.c
@@ -1,7 +1,18 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
-  *
-  *
-  *
+  * main - entry point
+  * @argc: takes in a string
+  * @argv: takes in array
+  * Return: 0
   */
+int main(int argc, char *argv[])
+{
+	int i;
 
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
