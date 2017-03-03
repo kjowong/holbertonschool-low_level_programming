c0eeb270c76132c7d042a6c664316dd7b1bf44e6
Kimberly Wong <kjowong@gmail.com>
2017-03-03 00:27:21 +0000

    Adding solution

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+  * wildcmp - compares two strings, returns 1 if identical
+  * @s1: takes in first string
+  * @s2: takes in 2nd string
+  * Return:
+  */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return wildcmp(s1 + 1, s2 + 1);
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+
+			return wildcmp(s1, s2 + 1);
+
+		}
+		if (*(s2 + 1) != '*')
+		{
+			return wildcmp(s1 + 1, s2);
+		}
+
+	}
+	return (0);
+}
