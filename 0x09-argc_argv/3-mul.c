47915531d2f04f7b9f67edd7e0a51f91bc95ee5a
Kimberly Wong <kjowong@gmail.com>
2017-03-04 00:23:42 +0000

    Adding 3

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,7 +1,31 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  *
-  *
-  *
+  * main - entry point
+  * @argc: takes in arguments
+  * @argv: argument array
+  * Return: 0
   */
+int main(int argc, char *argv[])
+{
 
+	int i, result;
+
+	result = 1;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result *= atoi(argv[i]);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
