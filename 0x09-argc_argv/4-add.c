31d80e97c1667b1c34ccc6ea6483d1aff5bf8b78
Kimberly Wong <kjowong@gmail.com>
2017-03-04 02:44:57 +0000

    Adding partial

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -1,7 +1,47 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
-  *
-  *
-  *
+  * main - entry point
+  * @argc: takes in arguments
+  * @argv: takes in arguments array
+  * Return: 0
   */
+int main(int argc, char *argv[])
+{
+	int i, result, counter;
 
+	result = 0;
+
+	/**if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	**/
+
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i] == 0))
+		{
+
+			result += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
+
+int _isdigit(char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (0);
+	}
+	return (1);
+}
