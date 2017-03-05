6ea7c476d31eea0031269f4dac779d03d8601a0c
Kimberly Wong <kjowong@gmail.com>
2017-03-05 01:18:08 +0000

    Refactored to handle negatives

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,19 +13,33 @@ int main(int argc, char *argv[])
 {
 	int i, j, result;
 
+
 	result = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		if (argv[i][0] == '-')
 		{
-			if (isdigit(argv[i][j]) == 0)
+			for (j = 1; argv[i][j] != '\0'; j++)
 			{
-				if (argv[i][0] != '-')
+				if (isdigit(argv[i][j]) == 0)
 				{
 					printf("Error\n");
 					return (1);
 				}
+
+			}
+		}
+		else
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+
 			}
 		}
 		result += atoi(argv[i]);
