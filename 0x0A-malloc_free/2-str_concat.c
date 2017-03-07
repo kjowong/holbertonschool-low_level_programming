06b001de520ef397fed5a017a257628199b4b2b8
Kimberly Wong <kjowong@gmail.com>
2017-03-07 23:49:25 +0000

    Solution before checker

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -1,9 +1,64 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
-  *
-  *
-  *
+  * _strlen - finds length of string
+  * @s: takes in string
+  * Return: string length in i
   *
   */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+  * str_concat - concats two strings
+  * @s1: takes in string
+  * @s2: takes in string
+  * Return: returns a pointer to nstr, followed by null
+  */
+char *str_concat(char *s1, char *s2)
+{
+	char *nstr;
+
+	unsigned int i, j, str1, str2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	else
+	{
+		str1 = _strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	else
+	{
+		str2 = _strlen(s2);
+	}
+
+	nstr = malloc((str1 * sizeof(char)) + (str2 * sizeof(char) + 1));
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		*(nstr + i) = *(s1 + i);
+	}
+	for (j = 0; s2[j] != '\0'; j++, i++)
+	{
+		*(nstr + i) = *(s2 + j);
+	}
+	nstr[i] = '\0';
+	return (nstr);
+}
