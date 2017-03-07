6a43f5796adee944905894d6114606fd7d0126b4
Kimberly Wong <kjowong@gmail.com>
2017-03-07 17:48:07 +0000

    Solution

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,9 +1,27 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
-  *
+  * create_array - func create an array of chars, initialized w/ specific char
+  * @size: size of the array
+  * @c: starting character
+  * Return: pointer to array, ar_ptr
   */
+char *create_array(unsigned int size, char c)
+{
+	char *ar_ptr;
+	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ar_ptr = malloc(size * sizeof(c));
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(ar_ptr + i) = c;
+	}
+	return (ar_ptr);
+}
