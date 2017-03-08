97ec06fdb99f836f5debb0f3f3e7f306f2083270
Kimberly Wong <kjowong@gmail.com>
2017-03-08 04:37:05 +0000

    Checking

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -44,10 +44,10 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < ac; i++)
+	for (i = 0, k = 0; i < ac; i++)
 	{
 
-		for (j = 0, k = 0; av[i][j] != '\0'; j++, k++)
+		for (j = 0; av[i][j] != '\0'; j++, k++)
 		{
 			nstr[k] = av[i][j];
 		}
