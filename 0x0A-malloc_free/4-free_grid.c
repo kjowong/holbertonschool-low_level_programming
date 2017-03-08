c0276e4aebbf4a3587fad41bc54da5284a3811c1
Kimberly Wong <kjowong@gmail.com>
2017-03-08 01:42:54 +0000

    Solution to free grid

--- 0x0A-malloc_free/4-free_grid.c
+++ 0x0A-malloc_free/4-free_grid.c
@@ -1,9 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
+  * free_grid - free grid space
+  * @grid: takes in the grid from #3
+  * @height takes in the height
   *
   */
+void free_grid(int **grid, int height)
+{
+	int i;
 
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
