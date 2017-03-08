d454a11691a7d4ce8966d146b69480400505a4b6
Kimberly Wong <kjowong@gmail.com>
2017-03-08 03:36:08 +0000

    Fixing j

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -18,7 +18,7 @@ char *argstostr(int ac, char **av)
 	}
 	for (i = 0; i < ac; i++)
 	{
-		for (i = 0, length = 0; av[i][j] != '\0'; j++, length++)
+		for (j = 0, length = 0; av[i][j] != '\0'; j++, length++)
 		{
 			;
 		}
