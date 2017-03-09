6680ab1213e469a2bcabd6ce571876cba069fe7e
Kimberly Wong <kjowong@gmail.com>
2017-03-09 20:03:32 +0000

    Adding files

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
+  * malloc_checked - a function that allocates memory using malloc
+  * @b: takes in an unsigned integer
+  * Return: returns a pointer to the allocated memory
   *
   */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *num;
+
+	num = malloc(sizeof(unsigned int) * b);
+
+	if (num == NULL)
+	{
+		exit(98);
+	}
+	return (num);
+}
--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,63 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
-  *
+  * _strlen - find length of a string
+  * @s: takes in a string
+  * Return: string length in i
   */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+  * string_nconcat - function that concats two strings
+  * @s1: takes in one string
+  * @s2: takes in another string
+  * @n: takes in an integer
+  * Return: returns a new string up to the num passed for str2
+  */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nstr, *empty = "";
+
+	unsigned int i, j, str1 = 0, str2 = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = empty;
+	}
+	else
+	{
+		str1 = _strlen(s1);
+	}
+	if (s2 == NULL)
+	{
+		s2 = empty;
+	}
+	else
+	{
+		str2 = _strlen(s2);
+
+	}
+	nstr = malloc((str1 * sizeof(char)) + (str2 * sizeof(char)) + 1);
+	if (nstr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		nstr[i] = s1[i];
+	}
+	for (j = 0; j < n && s2[j] != '\0'; j++, i++)
+	{
+		nstr[i] = s2[j];
+	}
+	nstr[i] = '\0';
+	return (nstr);
+}
