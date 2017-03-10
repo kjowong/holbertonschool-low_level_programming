a555cad4205cd76450ed204971d8e72421281292
Kimberly Wong <kjowong@gmail.com>
2017-03-10 04:09:28 +0000

    Add condition if new pointer is null

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -30,8 +30,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	unsigned int num_copied;
 
-	p = malloc(new_size);
-
 	if (new_size == old_size)
 	{
 		return (ptr);
@@ -45,6 +43,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	p = malloc(new_size);
 	if (new_size > old_size)
 	{
 		free(ptr);
@@ -55,5 +54,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		num_copied = old_size;
 	}
 	p = _memcpy(p, ptr, num_copied);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
 	return (p);
 }
