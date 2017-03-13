6d186253d163c74e518ba04d60969b48fe667c6e
Kimberly Wong <kjowong@gmail.com>
2017-03-13 20:12:24 +0000

    Adding comments

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -2,7 +2,15 @@
 #define HOLBERTON_H
 int _putchar(char c);
 #endif /* HOLBERTON_H */
-
+/**
+  * struct dog - dog structure
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  *
+  * Description: dog structure with 3 members
+  *
+  */
 struct dog
 {
 	char *name;
