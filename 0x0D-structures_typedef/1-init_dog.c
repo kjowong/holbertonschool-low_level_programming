80c37527b96cca2621546f00e83f475e4556715c
Kimberly Wong <kjowong@gmail.com>
2017-03-13 20:26:07 +0000

    Moving files

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+  * init_dog - function that initialize a variable of type struct dog
+  * @dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: owner of dog
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	typedef struct dog
+	{
+
+
+	}
+	
+}
