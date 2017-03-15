315b9ad526429a81357ea8f25058e88f5b30847a
Kimberly Wong <kjowong@gmail.com>
2017-03-15 21:28:24 +0000

    Adding solution

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  *
+  *
+  *
+  */
+int main(int argc, char **argv)
+{
+	int result;
+
+	result = 0;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	result = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", result);
+	return (0);
+}
