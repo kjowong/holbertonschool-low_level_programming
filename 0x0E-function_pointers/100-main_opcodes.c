90082d4247e4d64e289c6c8b6b70fd6887969e7e
Kimberly Wong <kjowong@gmail.com>
2017-03-16 21:13:27 +0000

    Adding Solution

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry block
+  * @argc: number of arguments
+  * @argv: arguments array
+  * Return: 0
+  */
+int main(int argc, char **argv)
+{
+	int i, arg;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	arg = atoi(argv[1]);
+
+	if (arg < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < arg; i++)
+	{
+		printf("%x ", ((unsigned char *)main)[i]);
+	}
+	printf("\n");
+	return (0);
+}
+
