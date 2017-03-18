cd4f46a4dfd41342e26908f1c25ee52dfd6f3303
Kimberly Wong <kjowong@gmail.com>
2017-03-18 19:28:14 +0000

    Adding header

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
+#include <stdio.h>
+/**
+  * struct op - Struct op
+  * @op: function being returned
+  * @f: function associated
+  */
+typedef struct op
+{
+	char *c;
+	void (*f)();
+
+}p_t;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
