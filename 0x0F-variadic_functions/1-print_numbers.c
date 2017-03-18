51a8eea3b3f9772be6e2c3ce27122a32a9c50411
Kimberly Wong <kjowong@gmail.com>
2017-03-18 19:37:18 +0000

    Removing null case, moved to if statement

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -12,16 +12,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		if (i < n - 1)
+		if (i != n - 1)
 		{
 			if (separator)
 			{
