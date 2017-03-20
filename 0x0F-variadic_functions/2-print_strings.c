25ef987c1c9ae22f42fddf360e1437b3bc8d4039
Kimberly Wong <kjowong@gmail.com>
2017-03-20 15:35:56 +0000

    Took out newline for nil case

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		temp = va_arg(valist, char *);
 		if (temp == NULL)
 		{
-			printf("(nil)\n");
+			printf("(nil)");
 		}
 		else
 		{
