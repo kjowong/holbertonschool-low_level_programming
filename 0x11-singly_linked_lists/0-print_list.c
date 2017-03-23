d2a8cadf9d61cc48459d607fa1c8420a21c9fc69
Kimberly Wong <kjowong@gmail.com>
2017-03-23 21:35:58 +0000

    Adding solution

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+  * print_list - function that prints the elements of a list
+  * @h: pointer to constat list_t
+  * Return: size_t
+  */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *walker = h;
+
+	if (walker == NULL)
+	{
+		return (0);
+	}
+	while (walker != NULL)
+	{
+		printf("[%d] %s\n", walker->len, walker->str);
+		walker = walker->next;
+		counter++;
+	}
+	return (counter);
+}
+
