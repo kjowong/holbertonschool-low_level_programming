7dba7093c42451cc0c7350d695f2a81c3c465a29
Kimberly Wong <kjowong@gmail.com>
2017-03-23 22:04:31 +0000

    Adding function to return elements

--- 0x11-singly_linked_lists/1-list_len.c
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -1,7 +1,23 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * list_len - function that returns the elements of a list
+  * @h: pointer to constat list_t
+  * Return: size_t
   */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+	const list_t *walker = h;
+
+	if (walker == NULL)
+	{
+		return (0);
+	}
+	while (walker != NULL)
+	{
+		walker = walker->next;
+		counter++;
+	}
+	return (counter);
+}
+
