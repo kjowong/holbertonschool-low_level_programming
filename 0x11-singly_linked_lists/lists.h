a749f05dee86e54e916f46729c48708ff986360b
Kimberly Wong <kjowong@gmail.com>
2017-03-23 21:36:45 +0000

    Adding header

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 #include <stdio.h>
-#include <stblib.h>
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,4 +18,5 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
 #endif /* LISTS_H */
