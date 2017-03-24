6e58ace15f56a957a8539762239ef9f3b87f7fe3
Kimberly Wong <kjowong@gmail.com>
2017-03-24 01:28:01 +0000

    Adding solution

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: takes in a string
+ * Return: i, string length
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+  * add_node_end - add node to the end
+  * @head: pointer to a pointer
+  * @str: takes in a string
+  * Return: pointer to the end
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newNode;
+	list_t *walker;
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	newNode->str = strdup(str);
+	newNode->len = _strlen(str);
+	if (*head == NULL)
+	{
+		*head = newNode;
+		newNode->next = NULL;
+	}
+	walker = malloc(sizeof(list_t));
+	if (walker == NULL)
+	{
+		return (NULL);
+	}
+	walker = *head;
+	while (walker->next != NULL)
+	{
+		walker = walker->next;
+	}
+	walker->next = newNode;
+	newNode->next = NULL;
+	return (newNode);
+}
