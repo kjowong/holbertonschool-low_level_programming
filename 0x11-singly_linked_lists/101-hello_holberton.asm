f70847d173fbdd271c4d3d55c504f6dd33f97966
Kimberly Wong <kjowong@gmail.com>
2017-03-25 22:16:04 +0000

    Adding assembly calling printf

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -2,18 +2,20 @@
 			section .text ; section with text
 			global  main ; declaring global main function, entry point
 
-main: ;assembling main function
-	        push    rbx	; push to stack
+			section .data
+msg: 	db "Hello, Holberton", 0 ;delare msg end in null
+fmt:	db "%s", 10, 0 ;declare format, follow by \n(10) and null
 
-	        mov     rdi, fmt	; move format to the register rdi
-	       	mov     rsi, msg ; move message to register rsi
-	        mov     rax, 1 ; run syscall 1 to write function
+main: ;assembling main function
+	        push    rbp	; push to stack
+
+	mov     rdi, fmt	; move format to the register rdi
+	mov     rsi, msg ; move message to register rsi
+	mov     rax, 1 ; run syscall 1 to write function
 	       	call    printf	 ; call the printf function
 
-			pop rbx	; pop stack
+			pop rbp	; pop stack
 
-			mov rax, 1 ;Exit to stdout
-			ret ; Return
-			section .data
-			msg: 	db "Hello, Holberton", 0 ;delare message end in null terminator
-			fmt:	db "%s", 10, 0 ;delcare format, follow by newline(10) and null terminator
+	mov rax, 1 ;Exit to stdout
+	ret ; Return
+
