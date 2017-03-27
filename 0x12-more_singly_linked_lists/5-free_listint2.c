84d733f03b0fb1f9d9436aa642a16df6db9bbd06
Kimberly Wong <kjowong@gmail.com>
2017-03-27 20:19:39 +0000

    Adding functions

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,17 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * free_listint2 - function that frees a listint_t list
+  * @head: pointer to a pointer
  */
+void free_listint2(listint_t **head)
+{
+	listint_t *walker;
 
+	while (*head != NULL)
+	{
+		walker = *head;
+		*head = walker->next;
+		free(walker);
+	}
+	*head = NULL;
+}
--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,24 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * get_nodeint_at_index - return the nth node starting at 0
+  * @head: takes in struct listint_t *head
+  * @index: takes in an index
+  * Return: the integer walker points to
  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *walker;
+	unsigned int counter = 0;
 
+	walker = head;
+	while (walker != NULL)
+	{
+		if (counter == index)
+		{
+			return (walker);
+		}
+		counter++;
+		walker = walker->next;
+	}
+	return (walker);
+}
