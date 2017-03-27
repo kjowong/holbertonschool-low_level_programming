e2714044a85c7660e3d47c116e1bb76a9d423189
Kimberly Wong <kjowong@gmail.com>
2017-03-27 17:03:23 +0000

    Adding header and functions

--- 0x12-more_singly_linked_lists/4-free_listint.c
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,17 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * free_listint - free the listint_t list
+  * @head: takes in a struct type listint_t *head
  */
+void free_listint(listint_t *head)
+{
+		listint_t *walker;
 
+		while (head != NULL)
+		{
+			walker = head;
+			head = walker->next;
+			free(walker);
+		}
+
+}
