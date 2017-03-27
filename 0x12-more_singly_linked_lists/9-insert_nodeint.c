85937adfa38cc263b73cbd64c3f697deae6c5c46
Kimberly Wong <kjowong@gmail.com>
2017-03-27 21:57:31 +0000

    Adding insert function

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,8 +1,35 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * insert_nodeint_at_index - inserts a new node at a given position
+  * @head: pointer to a pointer
+  * @idx: takes in the index
+  * @n: takes in an integer
+  * Return: NULL if function fail
  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode;
+	listint_t *temp;
+	unsigned int counter = 0;
 
+	if (head == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(listint_t));
+	newNode->n = n;
+	if (newNode == NULL)
+		return (NULL);
+	temp = *head;
+	while (temp != NULL)
+	{
+		if (counter + 1 == idx)
+		{
+			newNode->next = temp->next;
+			temp->next = newNode;
+			return (newNode);
+		}
+		temp = temp->next;
+		counter++;
+	}
+	free(newNode);
+	return (NULL);
+}
