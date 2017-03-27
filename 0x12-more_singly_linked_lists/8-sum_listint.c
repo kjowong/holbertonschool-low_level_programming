30bae4b3856b33d92718b31bb367522f6c2acac4
Kimberly Wong <kjowong@gmail.com>
2017-03-27 20:42:10 +0000

    Adding index function

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -1,8 +1,21 @@
 #include "lists.h"
 /**
-  *
-  *
-  *
-  *
+  * sum_listint - returns the sum of all the data of the linked list
+  * @head: takes in a struct type pointer *head
+  * Return: returns the sum of all the data
  */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
 
+	temp = head;
+
+	int sum = 0;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
