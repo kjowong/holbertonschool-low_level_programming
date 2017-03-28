5ece05eb413ae57e6f4627d2dba0a06f1a7b2b00
Kimberly Wong <kjowong@gmail.com>
2017-03-28 20:34:07 +0000

    Removing *head null case

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp;
 	unsigned int counter = 0;
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 		return (NULL);
 	newNode = malloc(sizeof(listint_t));
 	newNode->n = n;
