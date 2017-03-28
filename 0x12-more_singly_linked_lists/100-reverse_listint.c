d7320da01bec58f85ba98f7e40b2cd0f16cb800f
Kimberly Wong <kjowong@gmail.com>
2017-03-28 23:29:50 +0000

    Adding reverse function

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  * reverse_listint - reverses a linked list
+  * @head: pointer to a pointer
+  * Return: head to NULL
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+		listint_t *prevNode;
+		listint_t *nextNode;
+
+		prevNode = NULL;
+
+		while (*head != NULL)
+		{
+			nextNode = (*head)->next;
+			(*head)->next = prevNode;
+			prevNode = *head;
+			*head = nextNode;
+		}
+		*head = prevNode;
+		return (*head);
+}
