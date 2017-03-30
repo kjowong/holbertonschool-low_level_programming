3a9a180be7eb198c86111933ccf4d07ef1862ff0
Kimberly Wong <kjowong@gmail.com>
2017-03-30 20:55:57 +0000

    Adding set bit function

--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -1,8 +1,18 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
+  * set_bit - sets the value of a bit to 1 at given index
+  * @n: takes in a pointer to a number
+  * @index: the index of the bit you want
+  * Return: 1 or -1 if error
   */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int checkNum = 1;
 
+	checkNum <<= index;
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	(*n) = (*n) | checkNum;
+	return (1);
+}
