aa45a7b1428b2069f190d451bf3b0859f9b15e21
Kimberly Wong <kjowong@gmail.com>
2017-03-30 22:26:53 +0000

    Adding flip bits

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -1,8 +1,24 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
+  * flip_bits - # of bits you would need to flip to get fr one # to another
+  * @n: takes in an integer
+  * @m: takes in the 2nd integer to compare
+  * Return: number of bits
   */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int counter = 0;
+	int index = (sizeof(unsigned long int) * 8) - 1;
+	unsigned long int num = n ^ m;
 
+	while (index >= 0)
+	{
+		if (((num >> index) & 1) == 1)
+		{
+			counter += 1;
+		}
+		index--;
+	}
+	return (counter);
+}
