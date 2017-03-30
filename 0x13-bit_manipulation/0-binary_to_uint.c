9143339a636abc8e3f1ac587762758fafad74e07
Kimberly Wong <kjowong@gmail.com>
2017-03-30 18:37:24 +0000

    Adding binary to unit

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,30 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
-  *
-  *
-  *
+  * binary_to_uint - function that converts binary to unsigned int
+  * @b: takes in a conts char string
+  * Return: the converted unsigned int
   */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
+	unsigned int counter = 0;
+	unsigned int decimal = 0;
 
-	while (b != '\0')
-
-
-
+	if (b == NULL)
+		return (0);
+	while (b[counter] != '\0')
+	{
+		if (b[counter] != '0' && b[counter] != '1')
+		{
+			return (0);
+		}
+		decimal <<= 1;
+		if (b[counter] == '1')
+		{
+			decimal ^= 1;
+		}
+		counter++;
+	}
+	return (total);
 }
