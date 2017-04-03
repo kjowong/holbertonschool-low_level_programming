3aac5dbc379f0ba9cf9068506b22fe761033ec56
Kimberly Wong <kjowong@gmail.com>
2017-04-03 00:24:59 +0000

    Cleaning code

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-  * main - initial block
-  *
+  * main - entry block
+  * @void: no argument
   * Return: 0
   **/
 int main(void)
@@ -14,19 +14,16 @@ int main(void)
 		{
 			putchar(i / 10 + '0');
 			putchar(i % 10 + '0');
-
 			putchar(' ');
-
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
-				if (i != 98 || j != 99)
-				{
-					putchar(',');
-					putchar(' ');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
