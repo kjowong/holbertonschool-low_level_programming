dcc49409ef43e141d88751b73630eb0ea30a19f1
Kimberly Wong <kjowong@gmail.com>
2017-04-04 20:44:14 +0000

    Adding create file function

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+  * create_file - create a function that creates a file
+  * @filename: points to file that we will be receiving
+  * @text_content: the text that is being written
+  * Return: 1 if successfull
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int file = 0, writeFile = 0, closeFile, len;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
+	if (file == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		closeFile = close(file);
+		return (1);
+	}
+	writeFile = write(file, text_content, len);
+	if (writeFile == -1)
+		return (-1);
+	closeFile = close(file);
+	if (closeFile == -1)
+		return (-1);
+	return (1);
+}
