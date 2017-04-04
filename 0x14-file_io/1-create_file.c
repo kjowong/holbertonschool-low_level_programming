b0cb6b1c6a3df53232c2e124c8444d9aaf827e22
Kimberly Wong <kjowong@gmail.com>
2017-04-04 20:56:07 +0000

    Closing file in write

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -24,9 +24,12 @@ int create_file(const char *filename, char *text_content)
 			return (-1);
 		return (1);
 	}
-	writeFile = write(file, text_content, len - 1);
+	writeFile = write(file, text_content, len);
 	if (writeFile == -1)
+	{
+		closeFile = close(file);
 		return (-1);
+	}
 	closeFile = close(file);
 	if (closeFile == -1)
 		return (-1);
