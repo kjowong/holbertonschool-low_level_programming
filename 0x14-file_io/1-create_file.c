515c7660770f1ae4b34d3ed1026caceea741e5a5
Kimberly Wong <kjowong@gmail.com>
2017-04-04 21:01:27 +0000

    Moving filename if null to top

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -10,30 +10,24 @@ int create_file(const char *filename, char *text_content)
 	int file, writeFile, len;
 	mode_t mode = S_IRUSR | S_IWUSR;
 
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
 	if (filename == NULL)
 		return (-1);
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, mode);
 	if (file == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
 		close(file);
-		if (close(file) == -1)
-			return (-1);
 		return (1);
 	}
 	writeFile = write(file, text_content, len);
 	if (writeFile == -1)
 	{
 		close(file);
-		if (close(file) == -1)
-			return (-1);
 		return (-1);
 	}
 	close(file);
-	if (close(file) == -1)
-		return (-1);
 	return (1);
 }
