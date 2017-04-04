8e5e32d0bc4eab2de40528c19cb24865f37fa55b
Kimberly Wong <kjowong@gmail.com>
2017-04-04 20:48:39 +0000

    Checking if close is -1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -20,6 +20,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 	{
 		closeFile = close(file);
+		if (closeFile == -1)
+			return (-1);
 		return (1);
 	}
 	writeFile = write(file, text_content, len);
