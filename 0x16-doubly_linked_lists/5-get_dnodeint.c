c69c099284b8d013f1762be4c619f0487651dff8
Kimberly Wong <kjowong@gmail.com>
2017-05-04 22:31:52 +0000

    Adding function to get node

--- 0x16-doubly_linked_lists/5-get_dnodeint.c
+++ 0x16-doubly_linked_lists/5-get_dnodeint.c
@@ -1,2 +1,23 @@
 #include "lists.h"
+/**
+ * get_dnodeint_at_index - returns the nth node of a linked list
+ * @head: head of the linked list
+ * @index: index of the linked list
+ * Return: the nth node of the linked list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int counter = 0;
+
+	temp = head;
+	while (walker != NULL)
+	{
+		if (counter == index)
+			return (temp);
+	counter++;
+	temp = temp->next;
+	}
+	return (temp);
+}
 
