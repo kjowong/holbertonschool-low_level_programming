c6c9684320675b3b77ce5b94e73a58b43e35ae8a
Kimberly Wong <kjowong@gmail.com>
2017-05-04 22:41:02 +0000

    Adding function to count nodes

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,2 +1,21 @@
 #include "lists.h"
+/**
+  * sum_dlistint - sums up all the data of a linked list
+  * @head: head of the linked list
+  * Return: the sum
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+	unsigned int sum = 0;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
 
