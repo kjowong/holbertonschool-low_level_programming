f82f4194a41d82e1012d40870fcd425f19443661
Kimberly Wong <kjowong@gmail.com>
2017-06-04 15:21:53 +0000

    Adding null check)

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -30,6 +30,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 	new_node->next = NULL;
 	if (ptr == NULL)
 		ptr = new_node;
