dafa8116b9afd61700795da22995faf7aa15ebac
Kimberly Wong <kjowong@gmail.com>
2017-06-04 01:02:02 +0000

    Adding djb2 algorithm

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -1,1 +1,15 @@
-#include <hash_tables.h>
+#include "hash_tables.h"
+/**
+  * hash_djb2 - hash function that implements the djb2 algorithm
+  * @str: takes in the string
+  * Return: the hash
+  */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	while (c = *str++)
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return (hash);
+}
