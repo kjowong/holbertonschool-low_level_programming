cce060e017a9bd9e560dfe892f6e5c2dd853a26b
Kimberly Wong <kjowong@gmail.com>
2017-06-04 00:45:21 +0000

    change to callloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,23 +7,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
 	/* Allocate for size of hash_table */
-	hash_table = malloc(sizeof(hash_table_t));
+	hash_table = calloc(sizeof(hash_table_t), size);
 	if (hash_table == NULL)
 		return (NULL);
 	/* Allocate for the nodes in the array */
-	hash_table->array = malloc(sizeof(hash_node_t *));
+	hash_table->array = calloc(sizeof(hash_node_t *), size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
 	hash_table->size = size;
 	return (hash_table);
 }
