b765be7857e2690ea2635a8cc13b384c6012c9f8
Kimberly Wong <kjowong@gmail.com>
2017-06-04 18:02:22 +0000

    adding function without comma

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -1,1 +1,27 @@
-#include <hash_tables.h>
+#include "hash_tables.h"
+/**
+  * hash_table_print - function that prints a hash table
+  * @ht: the hash table
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	index = 0;
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (index < ht->size)
+	{
+		node = ht->array[index];
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			index++;
+		}
+	index++;
+	}
+	printf("}\n");
+}
