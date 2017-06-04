4d6e195dd61b617d39e751ebfda186703cb01a19
Kimberly Wong <kjowong@gmail.com>
2017-06-04 00:42:08 +0000

    Initialize all elements to NULL

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -7,6 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
@@ -21,6 +22,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 	hash_table->size = size;
 	return (hash_table);
 }
