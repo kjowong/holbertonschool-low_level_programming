f91a6ebb1e60460bfab67f1fb3a0b7824f35ddda
Kimberly Wong <kjowong@gmail.com>
2017-06-15 12:16:15 +0000

    initial commit

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+  * bubble_sort - sorts an array of ints in ascending order
+  * @array -
+  * @size -
+  * Return: 
+  */
+void bubble_sort(int *array, size_t size)
+{
+
+
+
+
+}
---
+++ 0x1A-sorting_algorithms/sort.h
@@ -0,0 +1,19 @@
+#ifndef SORT_H
+#define SORT_H
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+void bubble_sort(int *array, size_t size);
+#endif /* SORT_H */
