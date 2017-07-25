0e2db2a95ad79eb03a5b8f02779bb0d0ee4e2d70
Julija Lee <lee.julija@gmail.com>
2017-07-25 03:29:44 +0000

    optimized 18 with 17

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include "17-binary_tree_sibling.c"
 /**
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node to find the uncle of
@@ -6,9 +7,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent || !node->parent->parent)
+	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+	return (binary_tree_sibling(node->parent));
 }
