f5698546dade1ca198865c82762fa5a98ff53c36
Kimberly Wong <kjowong@gmail.com>
2017-07-26 20:27:09 +0000

    Removed extra statements in nodes

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -18,8 +18,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
-		newNode->parent = parent;
-		parent->left = newNode;
 		newNode->left->parent = newNode;
 	}
 	parent->left = newNode;
