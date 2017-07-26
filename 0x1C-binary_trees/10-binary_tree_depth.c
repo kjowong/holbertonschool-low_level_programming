637bedb78f91251c4e259b23fa59b0d802d58d49
Kimberly Wong <kjowong@gmail.com>
2017-07-26 20:54:13 +0000

    Adding null check

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -11,6 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *node)
 
 	depth = 0;
 
+	if (node == NULL)
+		return (0);
 	if (node && node->parent == NULL)
 		return (0);
 	depth = (binary_tree_depth(node->parent) + 1);
