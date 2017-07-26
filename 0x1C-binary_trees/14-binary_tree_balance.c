0bc56322736d9d0e282c8277c07b6e8c48a12555
Kimberly Wong <kjowong@gmail.com>
2017-07-26 19:23:21 +0000

    Changing height function and adding 14

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -7,7 +7,8 @@
   */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance_factor, left, right;
+	int balance_factor;
+	size_t left, right;
 
 	left = 0;
 	right = 0;
@@ -16,8 +17,33 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	left = custom_binary_height(tree->left);
+	right = custom_binary_height(tree->right);
 	balance_factor = left - right;
 	return (balance_factor);
+}
+/**
+  * custom_binary_height - finds height of tree and treats one node height as 1
+  * @tree: pointer to root node of the tree to measure
+  * Return: 1 if one node and height for other nodes
+  */
+int custom_binary_height(const binary_tree_t *tree)
+{
+	int l, r;
+
+	l = 0;
+	r = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	l = custom_binary_height(tree->left);
+	r = custom_binary_height(tree->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
+
+
 }
--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
-	if (tree->left > tree->right)
+	if (l > r)
 		return (l + 1);
 	else
 		return (r + 1);
