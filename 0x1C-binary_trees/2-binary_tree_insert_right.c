880724c03935be8e34403f098f637458c99853f4
Kimberly Wong <kjowong@gmail.com>
2017-07-26 20:30:57 +0000

    Adding NULL checks

--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -15,14 +15,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 	if (parent->right != NULL)
 	{
 		newNode->right = parent->right;
-		newNode->parent = parent;
-		parent->right = newNode;
 		newNode->right->parent = newNode;
 	}
 	parent->right = newNode;
-	newNode->parent = parent;
 	return (newNode);
 }
