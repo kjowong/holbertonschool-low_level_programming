00c810740bd22839e2db71170d050d64744c6aa7
Kimberly Wong <kjowong@gmail.com>
2017-07-26 20:29:09 +0000

    Adding null for left and right

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -15,12 +15,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 	if (parent->left != NULL)
 	{
 		newNode->left = parent->left;
 		newNode->left->parent = newNode;
 	}
 	parent->left = newNode;
-	newNode->parent = parent;
 	return (newNode);
 }
